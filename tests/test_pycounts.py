51001ce6b4c151d810146f08465f48a21a6859b7
samuelmacedo83 <svm.macedo@gmail.com>
2022-10-29 18:28:23 -0300

    test:add unit test for count_words

--- tests/test_pycounts.py
+++ tests/test_pycounts.py
@@ -2,23 +2,14 @@
 
 """Tests for `pycounts` package."""
 
-import pytest
+from pycounts.pycounts import count_words
+from collections import Counter
 
-
-from pycounts import pycounts
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_count_words():
+    """Test word counting from a file."""
+    expected = Counter({
+        'over': 2, 'and': 2, 'insanity': 1, 'is': 1, 'doing': 1,
+        'the': 1, 'same': 1, 'thing': 1, 'expecting': 1,
+        'different': 1, 'results': 1})
+    actual = count_words("tests/einstein.txt")
+    assert actual == expected, "Einstein quote counted incorrectly!"
