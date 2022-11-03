b130046fc052ef941913f2d0a2e6512eed511e09
samuelmacedo83 <svm.macedo@gmail.com>
2022-11-02 23:11:17 -0300

    test:add additional tests for all modules

--- tests/test_pycounts.py
+++ tests/test_pycounts.py
@@ -3,13 +3,61 @@
 """Tests for `pycounts` package."""
 
 from pycounts.pycounts import count_words
+from pycounts.plotting import plot_words
+from pycounts.datasets import get_flatland
 from collections import Counter
+import matplotlib 
+import pytest
 
-def test_count_words():
-    """Test word counting from a file."""
-    expected = Counter({
+@pytest.fixture
+def einstein_counts():
+    return Counter({
         'over': 2, 'and': 2, 'insanity': 1, 'is': 1, 'doing': 1,
         'the': 1, 'same': 1, 'thing': 1, 'expecting': 1,
         'different': 1, 'results': 1})
+
+def test_count_words(einstein_counts):
+    """Test word counting from a file."""
+    expected = einstein_counts
     actual = count_words("tests/einstein.txt")
     assert actual == expected, "Einstein quote counted incorrectly!"
+
+def test_plot_words(einstein_counts):
+    """Test plotting of words counts."""
+    counts = einstein_counts
+    fig = plot_words(counts)
+    assert isinstance(fig, matplotlib.container.BarContainer), \
+        "Wrong plot type"
+    assert len(fig.datavalues) == 10, \
+        "Incorrect number of bars plotted"
+    
+@pytest.mark.parametrize(
+    "obj",
+    [
+        3.141,
+        "test.txt",
+        ["list", "of", "words"]
+    ]
+)
+
+def test_plot_words_error(obj):
+    """Check TypeError raised when Counter not used."""
+    with pytest.raises(TypeError):
+        plot_words(obj)
+
+def test_integration():
+    """Test count_words() and plot_words() workflow"""
+    counts = count_words("tests/einstein.txt")
+    fig = plot_words(counts)
+    assert isinstance(fig, matplotlib.container.BarContainer), \
+        "Wrong plot type"
+    assert len(fig.datavalues) == 10, \
+        "Incorrect numbers of bars plotted"
+    assert max(fig.datavalues) == 2, \
+        "Highest word count should be 2"
+
+def test_regression():
+    """Regression test for Flatland"""
+    top_word = count_words(get_flatland()).most_common(1)
+    assert top_word[0][0] == "the", "Most common word is not 'the"
+    assert top_word[0][1] == 10, "the count has changed"
