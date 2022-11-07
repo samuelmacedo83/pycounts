c588dc838ab167af4999bc74ae18a675e7c0d554
samuelmacedo83 <svm.macedo@gmail.com>
2022-11-06 21:34:41 -0300

    build: added bumb version to __version__

--- pycounts/__init__.py
+++ pycounts/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Samuel Macedo"""
 __email__ = 'svm.macedo@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from pycounts.pycounts import count_words
 from pycounts.plotting import plot_words
